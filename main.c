59b70a32983c3de6a1e924b60b49ac4795ced55e
Michael Zhang <mzhang00@stuy.edu>
2020-01-10 11:41:30 -0500

    add prelim code

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+int main(int argc, char * argv[]){
+  while (1){
+    char name[500];
+    if (argc > 1) {
+      strcpy(name, argv[1]);
+    }else{
+      fgets(name, 500, stdin);
+      int i = 0;
+      while (name[i] != '\n') {
+        i++;
+      }
+      name[i] = '\0';
+  }
+  printf("%s\n", name);
+  return 0;
+}
