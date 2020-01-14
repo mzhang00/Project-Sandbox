8bf18f9c326f37822890baa2d7ef9bda1f2ab43b
Michael Zhang <mzhang00@stuy.edu>
2020-01-14 11:53:57 -0500

    t

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 #include <sys/stat.h>
 
 int main(int argc, char * argv[]){
+  int file;
   printf("Please enter your name: ");
   char nickname[500];
   fgets(nickname, 500, stdin);
@@ -29,7 +30,41 @@ int main(int argc, char * argv[]){
       }
       name[i] = '\0';
     }
-    printf("%s\n", name);
+
+    if (strcmp(name, "check") == 0){
+      //printf("test\n");
+      // file = open("log.txt", O_RDWR);
+      // //printf("test\n");
+      // char line [500];
+      // printf("%ld\n", read(file, line, 500));
+      // while (read(file, line, 500) > 0) {
+      //     printf("test\n");
+      //     printf("%s\n",line);
+      // }
+      // close(file);
+      FILE *f = fopen("log.txt", "r");
+      char str[500];
+      while (fgets(str, 500, f) != NULL){
+        printf("%s\n", str);
+      }
+      fclose(f);
+
+    }else{
+      FILE *f = fopen("log.txt", "a");
+
+      fprintf(f, "%s\n", name);
+
+      fclose(f);
+
+      //printf("%s\n", name);
+      // char line [500];
+      // read(file, line, 500);
+      // printf("%s\n",line);
+      //
+      // close(file);
+      //printf("%s\n", name);
+    }
+
   }
   // https://github.com/lovenery/c-chatroom/blob/master/src/client.c
   return 0;
