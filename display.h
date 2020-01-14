b9fa45540d2d4d80d01eb103520fae6aa3834a82
RAY2L <rlee00@stuy.edu>
2020-01-13 23:38:54 -0500

    gunscreen showing

--- display.h
+++ display.h
@@ -25,7 +25,7 @@ int loadMedia();
 //Frees media and shuts down SDL
 void close1(SDL_Renderer * rend,SDL_Texture * tex, SDL_Window * win);
 
-void render(SDL_Renderer* rend, SDL_Texture * tex, SDL_Rect *rect,SDL_Texture * tex2, SDL_Rect *rect2);
+void render(SDL_Renderer* rend, SDL_Texture * tex, SDL_Rect *rect,SDL_Texture * tex2, SDL_Rect *rect2, SDL_Texture * tex3, SDL_Rect * rect3);
 void clear(SDL_Renderer * renderer);
 
 #endif
