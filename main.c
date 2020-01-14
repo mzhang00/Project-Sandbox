24ee3fee270d72a512ef1a15483b810fb5e990e9
Chris Choi <mchoi00@stuy.edu>
2020-01-14 18:00:50 -0500

    added new map and stickman images

--- main.c
+++ main.c
@@ -5,12 +5,6 @@ int main(){
         printf("Failed to initialize\n");
       }
   else {
-        //Different Types of Modes:
-        //Start Mode- mainscreen.bmp - 0
-        //Movement Mode- movement screen -1
-        //Gun Mode- rifle.bmp - 2
-        //Gun Control Mode -3
-        int mode = 0;
         SDL_Window * win = SDL_CreateWindow( "Sandbox Wars", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
         if( win == NULL ){
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
@@ -21,44 +15,18 @@ int main(){
         SDL_Surface* surface;
         char cwd[100];
         getcwd(cwd, 100);
-        strcat(cwd, "/stickman.bmp");
+        strcat(cwd, "/man.bmp");
         //Load splash image
         surface = SDL_LoadBMP(cwd );
         if( surface == NULL ){
             printf( "Unable to load image %s! SDL Error: %s\n", cwd, SDL_GetError() );
         }
 
-        SDL_Surface* surface2 = NULL;
-        getcwd(cwd, 100);
-        strcat(cwd, "/map.bmp");
-        //Load splash image
-        surface2 = SDL_LoadBMP(cwd );
-        SDL_Texture* tex2 = SDL_CreateTextureFromSurface(rend, surface2);
-        SDL_Rect * background = malloc(sizeof(SDL_Rect));
-        SDL_QueryTexture(tex2, NULL, NULL, &(background->w), &(background->h));
-        background->x = 0;
-        background->y = 0;
-        background->w = SCREEN_WIDTH;
-        background->h = SCREEN_HEIGHT;
 
         // loads image to our graphics hardware memory.
         SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
         SDL_FreeSurface(surface);
         // clears main-memory
-        
-        getcwd(cwd, 100);
-        strcat(cwd, "/gunscreen.bmp");
-        //Load splash image
-        surface = SDL_LoadBMP(cwd );
-        SDL_Texture* tex3 = SDL_CreateTextureFromSurface(rend, surface);
-        SDL_Rect * gun_screen;
-        SDL_QueryTexture(tex3, NULL, NULL, &gun_screen->w, &gun_screen->h);
-        gun_screen->x = 110;
-        gun_screen->y = 380;
-        if( surface == NULL ){
-            printf( "Unable to load image %s! SDL Error: %s\n", cwd, SDL_GetError() );
-        }
-        SDL_FreeSurface(surface);
 
         // let us control our image position
         // so that we can move it with our keyboard.
@@ -70,8 +38,8 @@ int main(){
           if( rect+ i == NULL ){
               printf( "Unable to query texture %d! SDL Error: %s\n", i, SDL_GetError() );
           }
-          rect[i].w /= 3;
-          rect[i].h /= 3;
+          rect[i].w /= 5;
+          rect[i].h /= 6;
 
           // sets initial x-position of object
           rect[i].x = i * (SCREEN_WIDTH - rect[i].w)/6;
@@ -79,6 +47,40 @@ int main(){
           // sets initial y-position of object
           rect[i].y = (SCREEN_HEIGHT - rect[i].h) / 2;
         }
+
+
+        SDL_Rect * menus = malloc(4*sizeof(SDL_Rect));
+        SDL_Texture * menus_tex = malloc(4*sizeof(SDL_Rect));
+
+        // connects our texture with rect to control position
+        for (int i = 0; i < 6; i++) {
+          SDL_QueryTexture(tex, NULL, NULL, &(rect[i].w), &(rect[i].h));
+          if( rect+ i == NULL ){
+              printf( "Unable to query texture %d! SDL Error: %s\n", i, SDL_GetError() );
+          }
+          rect[i].w /= 5;
+          rect[i].h /= 6;
+
+          // sets initial x-position of object
+          rect[i].x = i * (SCREEN_WIDTH - rect[i].w)/6;
+
+          // sets initial y-position of object
+          rect[i].y = (SCREEN_HEIGHT - rect[i].h) / 2;
+        }
+
+        getcwd(cwd, 100);
+        strcat(cwd, "/map.bmp");
+        surface = SDL_LoadBMP(cwd);
+        SDL_Texture* tex2 = SDL_CreateTextureFromSurface(rend, surface);
+        SDL_Rect background;
+        SDL_QueryTexture(tex2, NULL, NULL, &background.w, &background.h);
+        background.x = 0;
+        background.y = 0;
+        background.w = SCREEN_WIDTH;
+        background.h = SCREEN_HEIGHT;
+        SDL_FreeSurface(surface);
+
+
         // controls annimation loop
         int close = 0;
 
@@ -138,12 +140,11 @@ int main(){
             if (rect.y < 0)
                 rect.y = 0;
 */
-            render(rend,tex,rect, tex2, background, tex3, gun_screen);
+            render(rend,tex,rect, tex2, &background);
         }
 
         // rectroy texture
         free(rect);
-        free(background);
         close1(rend,tex,win);
       }
 
