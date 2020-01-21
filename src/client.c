2f935295f176e12cbe7e2a1eb728976821d39440
Michael Zhang <mzhang00@stuy.edu>
2020-01-21 04:36:10 -0500

    t

--- src/client.c
+++ src/client.c
@@ -24,8 +24,7 @@ void recv_msg_handler() {
     while (1) {
         int receive = recv(sockfd, receiveMessage, LENGTH_SEND, 0);
         if (receive > 0) {
-            printf("\r%s\n", receiveMessage);
-            printf("\r%s\n", receiveMessage);
+            //printf("\r%s\n", receiveMessage);
             str_overwrite_stdout();
         } else if (receive == 0) {
             break;
@@ -112,7 +111,59 @@ int main()
             printf("\nBye\n");
             break;
         }
+        char name[500];
+        if (argc > 1) {
+        strcpy(name, argv[1]);
+        }else{
+        fgets(name, 500, stdin);
+        int i = 0;
+        while (name[i] != '\n') {
+            i++;
+        }
+        name[i] = '\0';
+        }
+
+        if (strcmp(name, "check") == 0){
+        //printf("test\n");
+        // file = open("log.txt", O_RDWR);
+        // //printf("test\n");
+        // char line [500];
+        // printf("%ld\n", read(file, line, 500));
+        // while (read(file, line, 500) > 0) {
+        //     printf("test\n");
+        //     printf("%s\n",line);
+        // }
+        // close(file);
+        FILE *f = fopen("log.txt", "r");
+        char str[500];
+        while (fgets(str, 500, f) != NULL){
+            printf("%s\n", str);
+        }
+        fclose(f);
+
+        }else if (strcmp(name, "clear") == 0){
+
+        FILE *f = fopen("log.txt", "w");
+        fclose(f);
+
+        }else{
+        FILE *f = fopen("log.txt", "a");
+
+        fprintf(f, "%s\n", strcat(strcat(name, " "), nickname));
+
+        fclose(f);
+
+        //printf("%s\n", name);
+        // char line [500];
+        // read(file, line, 500);
+        // printf("%s\n",line);
+        //
+        // close(file);
+        //printf("%s\n", name);
+        }
+
     }
+    
 
     close(sockfd);
     return 0;
